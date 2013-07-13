e3e84c32511ee2becf8657ad45f33d8b2c6874d9
EddieV2 <e_vartanessian@live.com>
2013-07-13 12:44:42 -0400

    first commit

---
+++ assets/h5bp/js/main.js
@@ -0,0 +1,20 @@
+ (function() {
+ 	var to;
+ 		function pieceHieghts(){
+ 			to = setTimeout(function(){
+ 				$(".pure-g-r").each(function(i,el){
+ 					var contentPieces = $(el).find(".dashboard-piece");
+ 					var max = 0;
+ 					contentPieces.css("min-height", "");
+ 					$.grep(contentPieces, function(el,i){
+ 						max = Math.max($(el).outerHeight(),max);
+ 					});
+ 					contentPieces.css("min-height", max);
+ 				});	
+ 			}, 400);
+ 		}
+ 		$(window).on("resize", function(){
+ 			clearTimeout(to);
+ 			pieceHieghts();
+ 		}).trigger("resize");
+ })());
